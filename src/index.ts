cf32ef7867b9a2e945f9baaf8e4051e5ee98bb41
Yossapol Witayanont <reef_3llen@hotmail.com>
2023-12-24 21:42:12 +0700

    feat: :rocket: swagger

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
-import swagger from "@elysiajs/swagger";
-import { Elysia } from "elysia";
+import swagger from '@elysiajs/swagger';
+import { Elysia } from 'elysia';
+
+import './setup/database';
+import swaggerSetup from './setup/swagger';
 
 const app = new Elysia()
-  .use(
-    swagger({
-      documentation: {
-        info: {
-          title: "E-Commerce API",
-          version: "1.0.0",
-        }
-      }
-    })
-  )
-  .get("/", () => "Hello Elysia").listen(3000);
+  .use(swagger(swaggerSetup))
+  .get('/', () => 'Hello Elysia').listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
---
+++ src/setup/swagger.ts
@@ -0,0 +1,9 @@
+export default {
+  path: '/swagger',
+  documentation: {
+    info: {
+      title: 'E-Commerce API',
+      version: '1.0.0',
+    },
+  },
+}
